319ee86875926c11e09f0ab8f93ddd726932415b
Yunus Emre OCAK <68605316+yeocak@users.noreply.github.com>
2020-10-27 22:52:42 +0300

    First upload

---
+++ kotlin/en_uzun_aralik.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val text = readLine()!!.toLowerCase()
+
+    var kelime = ""
+
+    for(a in 0..text.length-2){
+
+        for(e in a+1..text.length-2){
+
+            for(i in e+1..text.length-1){
+
+                if(text.substring(e+1, i+1).contains(text.substring(a, e+2))){
+                    if(kelime.length < text.substring(a, e+2).length){
+                        kelime = text.substring(a, e+2)
+                    }
+                }
+
+            }
+
+        }
+
+    }
+    if(kelime == ""){
+        print("None")
+    }
+    else{
+        print(kelime)
+    }
+}
